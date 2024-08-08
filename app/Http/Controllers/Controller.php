d55e334452a6c5805e85afebe5c8dfda7b29db05
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2024-08-08 17:25:10 -0300

    feat: add swagger documentation from api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,6 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+use OpenApi\Attributes as OA;
+
+#[
+    OA\Info(version: "1.0", description: "Challenger API", title: "Challenger API"),
+    OA\Server(url: 'http://127.0.0.1', description: 'challenger-api server'),
+]
 
 abstract class Controller
 {
