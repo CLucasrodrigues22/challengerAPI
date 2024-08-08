c08b8ddec66dae919475ec9d2f737e69448e3edf
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2024-08-08 17:30:16 -0300

    feat: add factory and seeder to location

--- database/factories/LocationFactory.php
+++ database/factories/LocationFactory.php
@@ -17,7 +17,16 @@ class LocationFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->text(rand(5, 10)),
+            'slug' => $this->faker->slug(),
+            'city' => $this->faker->city(),
+            'state' => $this->faker->randomElement([
+                'Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal',
+                'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul',
+                'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro',
+                'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'São Paulo',
+                'Sergipe', 'Tocantins'
+            ])
         ];
     }
 }
